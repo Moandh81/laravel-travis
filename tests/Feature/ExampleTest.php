5c1cd51e0f7ef65a786fad72ca70ee72ec386ef9
mohand81 <anis.dhouieb@gmail.com>
2019-06-04 20:38:40 +0200

    remove function from test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,12 +14,12 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
-    // {
-    //     $response = $this->get('/');
-    //
-    //     $response->assertStatus(200);
-    // }
+    // public function testBasicTest()
+    // // {
+    // //     $response = $this->get('/');
+    // //
+    // //     $response->assertStatus(200);
+    // // }
 
 
 
